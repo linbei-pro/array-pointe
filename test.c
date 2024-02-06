bff6a2d915d89a5e08e76b78fe5744c92a8fa449
linbei-pro <148841405+linbei-pro@users.noreply.github.com>
2024-02-06 17:16:59 +0800

    Update test.c

--- test.c
+++ test.c
@@ -10,6 +10,7 @@ void print1(int(*p)[5], int r, int c)
 		for (j = 0; j < c; j++)
 		{
 			printf("%d ", *(*(p + i) + j));
+			//也可以写成 printf("%d ",p[i][j]);  *（p+i）——> p[i]
 		}
 		printf("\n");
 	}
@@ -21,6 +22,7 @@ int main() {
 	int arr[3][5] = { 1,2,3,4,5,2,3,4,5,6,3,4,5,6,7 };
 
 	print1(arr, 3, 5);
+	//传arr ，因为arr是一个二维数组，二维数组名表示的是整个第一行的地址，所以在函数声明时，要用一个数组指针来进行接收。
 
 	return 0;
 }
